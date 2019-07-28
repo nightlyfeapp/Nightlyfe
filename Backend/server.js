2dde604927e781b5b808eee97f2cefa4f1c874fd
KhallilB <khallil.bailey@gmail.com>
2019-07-28 12:48:22 -0700

    server

---
+++ Backend/server.js
@@ -0,0 +1,35 @@
+// Dependencies
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+require('dotenv').config();
+
+const server = express();
+
+// Middleware
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({ extended: true }));
+
+server.use(cors());
+
+// Database Connection
+mongoose
+  .connect(process.env.DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true
+  })
+  .then(() => {
+    console.log('Connected To Database!');
+  })
+  .catch(err => {
+    console.log(err);
+  });
+
+// Server
+const port = process.env.PORT;
+
+server.listen(port, () => {
+  console.log(`Server is listening on localhost: ${port}`);
+});

528b671df8dc976aa0ba9ca677ec58d7b137340b
KhallilB <khallil.bailey@gmail.com>
2019-07-28 13:30:00 -0700

    stubbing events controller

---
+++ Backend/controllers/events.js
@@ -0,0 +1,34 @@
+const Event = require('../models/Event');
+
+// Create new Event
+const newEvent = async (req, res) => {
+  // Code
+};
+
+// Gets an Event
+const getEvent = async (req, res) => {
+  // Code
+};
+
+// Gets all Events
+const allEvents = async (req, res) => {
+  // Code
+};
+
+// Updates an Event
+const updateEvent = async (req, res) => {
+  // Code
+};
+
+// Deletes an Event
+const deleteEvent = async (req, res) => {
+  // Code
+};
+
+module.exports = {
+  newEvent,
+  getEvent,
+  allEvents,
+  updateEvent,
+  deleteEvent
+};

84295428830ae8739382d13c4575cb520ab11e5d
KhallilB <khallil.bailey@gmail.com>
2019-07-30 14:54:08 -0700

    corrupted files

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -1,30 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import appRoutes from './routerConfig';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LandingComponent } from './components/landing/landing.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { LoginComponent } from './components/login/login.component';
-import { SignupComponent } from './components/signup/signup.component';
-import { UserProfileComponent } from './components/user-profile/user-profile.component';
-import { AllEventsComponent } from './components/all-events/all-events.component';
-import { EventComponent } from './components/event/event.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    LandingComponent,
-    NavbarComponent,
-    LoginComponent,
-    SignupComponent,
-    UserProfileComponent,
-    AllEventsComponent,
-    EventComponent
+    AppComponent
   ],
-  imports: [BrowserModule, RouterModule.forRoot(appRoutes)],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }

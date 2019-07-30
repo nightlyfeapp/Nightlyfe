07f33b7a1ce113be7203e9ea5539235677744210
KhallilB <khallil.bailey@gmail.com>
2019-07-30 10:57:49 -0700

    added components folder

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -2,13 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { LandingComponent } from './landing/landing.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { LoginComponent } from './login/login.component';
-import { SignupComponent } from './signup/signup.component';
-import { UserProfileComponent } from './user-profile/user-profile.component';
-import { AllEventsComponent } from './all-events/all-events.component';
-import { EventComponent } from './event/event.component';
+import { LandingComponent } from './components/landing/landing.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { UserProfileComponent } from './components/user-profile/user-profile.component';
+import { AllEventsComponent } from './components/all-events/all-events.component';
+import { EventComponent } from './components/event/event.component';
 
 @NgModule({
   declarations: [

4cd0a7ea6010d4543c298e7cb4f4bcff3a26289b
KhallilB <khallil.bailey@gmail.com>
2019-08-05 12:41:10 -0700

    working on getting all events

--- Frontend/src/components/events/allEvents/AllEvents.js
+++ Frontend/src/components/events/allEvents/AllEvents.js
@@ -3,7 +3,14 @@ import './AllEvents.css';
 
 class AllEvents extends Component {
   render() {
-    return <div>Getting All Events</div>;
+    return (
+      <div class="container">
+        <div class="section">
+          <h1 class="bp3-heading">Here's Some Events</h1>
+          <div class="events" />
+        </div>
+      </div>
+    );
   }
 }
 

21f76a62540ac5601d11e6a97540f10ea0b96cfd
KhallilB <khallil.bailey@gmail.com>
2019-08-05 15:21:12 -0700

    getting events

--- Frontend/src/components/events/allEvents/AllEvents.js
+++ Frontend/src/components/events/allEvents/AllEvents.js
@@ -1,13 +1,43 @@
 import React, { Component } from 'react';
 import './AllEvents.css';
 
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+
 class AllEvents extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      events: []
+    };
+  }
+
+  async componentDidMount() {
+    await axios.get('http://localhost:9999/events').then(res => {
+      const events = res.data.events;
+      console.log(events);
+      this.setState({ events });
+    });
+  }
+
   render() {
     return (
       <div class="container">
-        <div class="section">
-          <h1 class="bp3-heading">Here's Some Events</h1>
-          <div class="events" />
+        <h1 class="bp3-heading">Events</h1>
+        <div class="events">
+          <div className="card-container">
+            {this.state.events.map((events, index) => {
+              return (
+                <div key={events._id} class="bp3-card bp3-elevation-3">
+                  <Link to={'/events/:eventId'}>
+                    <h1>{events.title}</h1>
+                  </Link>
+                  <h3>{events.location}</h3>
+                  <h3>{events.description}</h3>
+                </div>
+              );
+            })}
+          </div>
         </div>
       </div>
     );

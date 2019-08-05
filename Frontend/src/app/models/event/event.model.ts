8146f6ad6967f498376bc8110d9dbcfaebb3bb69
KhallilB <khallil.bailey@gmail.com>
2019-08-05 08:15:32 -0700

    react

--- Frontend/src/app/models/event/event.model.ts
+++
@@ -1,9 +0,0 @@
-import { User } from '../user/user.model';
-
-export class Event {
-  title: string;
-  timeOfEvent: Date;
-  location: string;
-  description: string;
-  createdBy: User;
-}
--- Frontend/src/app/models/user/user.model.ts
+++
@@ -1,8 +0,0 @@
-import { Event } from '../event/event.model';
-
-export class User {
-  username: string;
-  email: string;
-  password: string;
-  eventsCreated: [Event];
-}

e33ed6e8bb6f11c2eac02ff4f378ddf68e7b5b5d
KhallilB <khallil.bailey@gmail.com>
2019-08-06 09:28:54 -0700

    login works

--- src/components/auth/login/Login.js
+++ src/components/auth/login/Login.js
@@ -2,19 +2,64 @@ import React, { Component } from 'react';
 import './Login.css';
 
 import { Button } from '@blueprintjs/core';
+import axios from 'axios';
 
 class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      username: '',
+      password: ''
+    };
+  }
+
+  submitForm = event => {
+    event.preventDefault();
+
+    const { username, password } = this.state;
+
+    axios
+      .post('https://nightlyfe-server.herokuapp.com/auth/login', {
+        username,
+        password
+      })
+      .then(res => {
+        console.log(res);
+      });
+  };
+
+  handleChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  };
+
   render() {
+    const { username, password } = this.state;
     return (
       <div class="container">
         <div class="login-form ">
           <h1> Log In</h1>
           <div class="bp3-input-group .modifier">
-            <input type="username" class="bp3-input" placeholder="Username" />
+            <input
+              type="username"
+              class="bp3-input"
+              placeholder="Username"
+              name="username"
+              value={username}
+              onChange={this.handleChange}
+            />
             <button class="bp3-button bp3-minimal bp3-intent-warning bp3-icon-person" />
           </div>
           <div class="bp3-input-group .modifier">
-            <input type="password" class="bp3-input" placeholder="Password" />
+            <input
+              type="password"
+              class="bp3-input"
+              placeholder="Password"
+              name="password"
+              value={password}
+              onChange={this.handleChange}
+            />
             <button class="bp3-button bp3-minimal bp3-intent-warning bp3-icon-lock" />
           </div>
           <Button rightIcon="arrow-right" intent="success" text="Log In" />

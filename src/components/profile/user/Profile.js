d2ab191c9ecfea4d7f304147cbeea2729e2662a8
KhallilB <khallil.bailey@gmail.com>
2019-08-07 12:21:29 -0700

    getting user data

--- src/components/profile/user/Profile.js
+++ src/components/profile/user/Profile.js
@@ -3,14 +3,30 @@ import './Profile.css';
 
 import avatar from '../../../assets/images/8c46965de03827dc2a05e4c77eb110db--batman-comics.jpg';
 import { Card, Elevation } from '@blueprintjs/core';
+import AuthService from '../../services/auth/AuthService';
+import { Link } from 'react-router-dom';
 
 class Profile extends Component {
   constructor() {
     super();
     this.state = {};
+    this.Auth = new AuthService();
+  }
+
+  getUser() {
+    let data = this.Auth.getProfile();
+    return data;
+  }
+
+  getRegistrationDate() {
+    let date = this.Auth.getProfile().subject.createdAt;
+    return date.substring(5, 7) + '/' + date.substring(0, 4);
   }
 
   render() {
+    const username = this.getUser().subject.username;
+    const date = this.getRegistrationDate();
+    const events = this.getUser().subject.eventsCreated;
     return (
       <div className="profile-container">
         <div className="profile-top" id="top">
@@ -19,8 +35,8 @@ class Profile extends Component {
             elevation={Elevation.TWO}
             className="profile-card">
             <img src={avatar} alt="" />
-            <p>Username</p>
-            <p>User Since</p>
+            <p>{username}</p>
+            <p>User Since: {date}</p>
           </Card>
         </div>
         <div className="profile-bottom" id="bottom">
@@ -37,13 +53,15 @@ class Profile extends Component {
                 <label htmlFor={'tabbed1'}>Events</label>
               </h1>
               <div>
-                <ul>
-                  <li>List Item</li>
-                  <li>List Item</li>
-                  <li>List Item</li>
-                  <li>List Item</li>
-                  <li>List Item</li>
-                </ul>
+                {events.map((events, index) => {
+                  return (
+                    <div key={index}>
+                      <Link to={'/events/:eventId'}>
+                        <h1>{events.title}</h1>
+                      </Link>
+                    </div>
+                  );
+                })}
               </div>
             </section>
 
@@ -58,17 +76,7 @@ class Profile extends Component {
               <h1>
                 <label htmlFor={'tabbed2'}>Attending</label>
               </h1>
-              <div>
-                <ul>
-                  <li>List Item</li>
-                  <li>List Item</li>
-                  <li>List Item</li>
-                  <li>List Item</li>
-                  <li>List Item</li>
-                  <li>List Item</li>
-                  <li>List Item</li>
-                </ul>
-              </div>
+              <div />
             </section>
           </div>
         </div>
